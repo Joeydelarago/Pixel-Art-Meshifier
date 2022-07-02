f2bdba2adf404f801005bd3994c5f1cdd695c624
joeydelarago <joeydelarago@gmail.com>
2022-07-02 10:55:03 +0200

    Add output file path option

--- src/main.py
+++ src/main.py
@@ -6,13 +6,14 @@ from mesh_generating_utils import create_backplate, create_mesh
 
 @click.command()
 @click.argument("image_path")
+@click.argument("output_path", default="pixels.stl")
 @click.option("-f", "--flat", is_flag=True, default=False)
 @click.option("-i", "--invert", is_flag=True, default=False)
 @click.option("-mx", "--max_height", default=255)
 @click.option("-b", "--backplate", is_flag=True, default=False)
-def create_models(image_path: str, invert: bool, flat: bool, max_height: int, backplate: bool) -> None:
+def create_models(image_path: str, output_path: str, invert: bool, flat: bool, max_height: int, backplate: bool) -> None:
     pixels_mesh: Trimesh = create_mesh(image_path, invert, flat, max_height, False)
-    pixels_mesh.export("pixels.stl")
+    pixels_mesh.export(output_path)
 
     if backplate:
         backplate_mesh: Trimesh = create_mesh(image_path, False, True, 1, True)
